8eaca91bc6b5132f35d840b1619a0cdad0e7193a
Zakky <khanthtoothu08@gmail.com>
2025-10-09 23:03:23 +0630

    zakky

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-
-
-
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('two-factor-verify/', views.two_factor_verify, name='two_factor_verify'),
     path('admin/home/', views.admin_home, name='admin_home'),
     path('signup/', views.signup_view, name='signup'),
     path('teacher/home/', views.teacher_home, name='teacher_home'),
@@ -14,6 +12,4 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('test/', views.test_view),
     path('logout/', views.logout_view, name='logout'),
-
-
 ]
